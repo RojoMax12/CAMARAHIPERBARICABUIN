94e21138bfd93a5b14fd721769a1e1d1e05aa85c
RojoMax12 <johangato@live.com>
2023-04-13 14:25:53 -0400

    Se quito la funcion abrirarchivoescribir, se modifico la funcion encontrar persona la cual ahora lee las lineas del archivo y las retorna para mostrar la cantidad de datos del paciente con el rut indicado y ahora muestra cuando no hay un paciente registrado con el rut indicado

--- Archivos.h
+++ Archivos.h
@@ -11,14 +11,12 @@ typedef struct Paciente
 
 /*Funciones creadas para los archivos*/
 FILE *abrirarchivoleer();
-FILE *abrirarchivoescrib();
 FILE *abrirarchivosobrescribir();
 
 /*Funciones para asignar memoria a la estructura Tipo paciente*/
 Pas *asignarmemoria(int n);;
 
 /*Funciones que se utilizaran para las diferentes opciones del pograma*/
-void ordenar_datos(FILE *p, FILE *q, int n, Pas *arrpas);
 void escribirenelarchivo(FILE *p, int n, Pas *arrpas);
 void encontrarpersona();
 
@@ -38,27 +36,12 @@ FILE *abrirarchivoleer(){
   return p;
 }
 
-FILE *abrirarchivoescrib(){
-  FILE *p;
-  char nombre[30];
-  printf("Ingrese el nombre del archivo de salida: ");
-  scanf("%s", nombre);
-  p = fopen(nombre, "w+");
-  if(p != NULL){
-    printf("El archivo a sido correctamente creado\n");
-  }
-  else{
-    printf("Error al crear el archivo el archivo\n");
-  }
-  return p;
-}
-
 FILE *abrirarchivosobrescribir(){
   FILE *p;
   char nombre[30];
   printf("Ingrese el nombre del archivo: ");
   scanf("%s", nombre);
-  p = fopen(nombre, "a+");
+  p = fopen(nombre, "a");
   if(p != NULL){
     printf("El archivo a sido correctamente abierto para escribir\n");
   }
@@ -84,44 +67,33 @@ Pas *asignarmemoria(int n){
     return a;    
 }
 
-/*Funcion que toma una archivo p y un archivo q el cual, lee el archivo p toma todos sus datos y los copia al archivo q ordenandolos por
-Nombre - Rut - Fecha - Plan - Valor*/
-void ordenar_datos(FILE *p, FILE *q, int n, Pas *arrpas){
-    int i = 0;
-    while(fscanf(p,"%s %s %d %d %d %d %d", arrpas[i].nombres, arrpas[i].rut, &arrpas[i].dia, &arrpas[i].mes, &arrpas[i].ano, &arrpas[i].plan, &arrpas[i].valor)!=EOF)
-    {
-      fprintf(q,"\nNombre: %s Rut: %s Fecha: %d/%d/%d Plan: %d sesiones Valor:$ %d",arrpas[i].nombres, arrpas[i].rut, &arrpas[i].dia, arrpas[i].mes, arrpas[i].ano, arrpas[i].plan, arrpas[i].valor);
-      i++;
-    }
-}
-
-/*Funcion todavia no terminanda, falta por revisar*/
+/*Funcion que escribe los datos del paciente en la linea final del archivo*/
 void escribirenelarchivo(FILE *p, int n, Pas *arrpas){
-  int i = 0, count = 0;
-  while (fscanf(p,"%s %s %d %d %d %d %d", arrpas[i].nombres, arrpas[i].rut, &arrpas[i].dia, &arrpas[i].mes, &arrpas[i].ano, &arrpas[i].plan, &arrpas[i].valor)!= EOF)
-  { i++;
-    count++;
-  }
-  fprintf(p, "%d", count + n);
-  /*
   for (int i = 0; i <= n - 1; i++)
-  {
-  fscanf(p,"%s %s %d %d %d %d %d", arrpas[i].nombres, arrpas[i].rut, &arrpas[i].dia, &arrpas[i].mes, &arrpas[i].ano, &arrpas[i].plan, &arrpas[i].valor);
-  fprintf(p,"\n %s %s %d %d %d %d %d",arrpas[i].nombres, arrpas[i].rut, arrpas[i].dia, arrpas[i].mes, arrpas[i].ano, arrpas[i].plan, arrpas[i].valor);
-  
-  printf("Ingrese los datos del paciente de la siguiente manera (NombreApellido RutSinPutosYconGuion Dia Mes Año PlanElCualEligio ValorDelPlan) ");
-  scanf("%s %s %d %d %d %d %d", arrpas[i].nombres, arrpas[i].rut, &arrpas[i].dia, &arrpas[i].mes, &arrpas[i].ano, &arrpas[i].plan, &arrpas[i].valor);
-  fprintf(p,"\n %s %s %d %d %d %d %d",arrpas[i].nombres, arrpas[i].rut, arrpas[i].dia, arrpas[i].mes, arrpas[i].ano, arrpas[i].plan, arrpas[i].valor);
-  }*/
+    {
+    printf("Ingrese los datos del paciente de la siguiente manera (NombreApellido RutSinPutosYconGuion Dia Mes Año PlanElCualEligio ValorDelPlan):  ");
+    scanf("%s %s %d %d %d %d %d", arrpas[i].nombres, arrpas[i].rut, &arrpas[i].dia, &arrpas[i].mes, &arrpas[i].ano, &arrpas[i].plan, &arrpas[i].valor);
+    fprintf(p,"\n%s %s %d %d %d %d %d",arrpas[i].nombres, arrpas[i].rut, arrpas[i].dia, arrpas[i].mes, arrpas[i].ano, arrpas[i].plan, arrpas[i].valor);
+  }
 }
 
 /*Funcion que busca en el archivo al paciente por su rut e imprime sus datos por pantalla*/
 void encontrarpersona(FILE *p, int n, Pas *arrpas){
-    char rut[12];
-    int cont = 0;
+    char rut[12], ca[50];
+    int cont = 0, con2 = 0;
+    while (!feof(p))
+    {
+    fgets(ca, 50, p);
+    con2++;
+    }
+
+    rewind(p);
+    asignarmemoria(con2);
+    arrpas = (Pas*)malloc(sizeof(Pas)*con2);
+
     printf("Ingrese el rut del paciente que desea buscar: ");
     scanf("%s", rut);
-    for(int i = 0; i <= n - 1; i++)
+    for(int i = 0; i <= con2 - 1; i++)
     {
       fscanf(p,"%s %s %d %d %d %d %d", arrpas[i].nombres, arrpas[i].rut, &arrpas[i].dia, &arrpas[i].mes, &arrpas[i].ano, &arrpas[i].plan, &arrpas[i].valor);
       if(strcmp(rut, arrpas[i].rut) == 0)
@@ -131,5 +103,8 @@ void encontrarpersona(FILE *p, int n, Pas *arrpas){
       }
       
     }
-    
+    if (cont == 0)
+    {
+      printf("No existe el paciente\n");
+    }  
 }
